319aaf27aa6668d6ab37cb19276f972326e044bd
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-08-23 19:19:30 +0200

    add list train

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    public function getDetails(){
+        return $this->azienda . ' / ' . $this->stazione_di_partenza . ' / ' . $this->stazione_di_arrivo . ' / ' . $this->orario_di_partenza . ' / ' . $this->orario_di_arrivo . ' / ' . $this->codice_treno . ' / ' . $this->numero_carrozze . ' / ' . $this->in_orario . ' / ' . $this->cancellato;
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <ul>
+        @foreach ($trains as $train)
+            <li class="fs-4">{{ $train->getDetails() }}</li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
