f0ffaba7cfc2ca121ac5d29e26f2362351ec5bb1
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-08-23 20:15:41 +0200

    add TrainSeeder (faker to do fix)

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for($i = 0; $i < 10; $i++){
+
+            $train = new Train();
+            
+            $train->azienda = $faker->company();
+            $train->stazione_di_partenza = $faker->city();
+            $train->stazione_di_arrivo = $faker->city();
+            $train->orario_di_partenza = $faker->time();
+            $train->orario_di_arrivo = $faker->time();
+            $train->codice_treno = $faker->randomNumber(10, true);
+            $train->numero_carrozze = $faker->rand(7,8);
+            $train->in_orario = $faker->rand(0,1);
+            $train->cancellato = $faker->rand(0,1);
+            
+            $train->save();
+        }
+
+    }
+}
